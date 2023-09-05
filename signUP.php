d1926629de575c59791bc6fbec495910060cf916
Filipe Pimenta <95368421+Lipepimenta@users.noreply.github.com>
2023-09-05 16:17:20 -0300

    Add files via upload

---
+++ signUP.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sign UP</title>
+    <link rel="stylesheet" href="./singUP.css">
+</head>
+<body>
+    <div class="form">
+        <form action="" method="post">
+            <div class="title">
+                <p>Sing UP form</p>
+            </div>
+            <input type="text" name="name" placeholder="Name">
+            <input type="text" name="last name" placeholder="Last name">
+            <input type="text" name="email" placeholder="Email">
+            <input type="text" name="password" placeholder="Password">
+            <input type="text" name="confiPassword" placeholder="Confirm Password">
+            <input type="submit" value="Sing UP" name="singUP_button">
+        </form>
+    </div>
+</body>
+</html>
